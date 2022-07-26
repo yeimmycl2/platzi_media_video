361ec0fe5b2b257206f6b6c53245b6ced6f30101
yeimmy cañon <yeimmycanon@yeimmys-MacBook-Air.local>
2022-07-26 18:45:50 -0500

    mute video

--- assets/index.js
+++ assets/index.js
@@ -1,25 +1,13 @@
-function MediaPlayer(config) {
-  this.media = config.el;
-}
-
-MediaPlayer.prototype.play = function () {
-  this.media.play();
-};
-
-MediaPlayer.prototype.pause = function () {
-  this.media.pause();
-};
-
-MediaPlayer.prototype.togglePlay = function () {
-  if (this.media.paused) {
-    this.play();
-  } else {
-    this.pause();
-  }
-};
+import MediaPlayer from "./MediaPlayer.js";
+import Autoplay from "./plugins/Autoplay.js";
 
 const video = document.querySelector("video");
-const player = new MediaPlayer({ el: video });
+const player = new MediaPlayer({ el: video, plugins: [new Autoplay()] });
 
 const button = document.querySelector("button");
 button.onclick = () => player.togglePlay();
+
+const buttonMute = document.querySelector("#mute__button");
+buttonMute.onclick = () => player.toggleMute();
+
+export default MediaPlayer;
---
+++ assets/plugins/AutoPlay.js
@@ -0,0 +1,8 @@
+function Autoplay() {}
+
+Autoplay.prototype.run = function (player) {
+  player.mute();
+  player.play();
+};
+
+export default Autoplay;
